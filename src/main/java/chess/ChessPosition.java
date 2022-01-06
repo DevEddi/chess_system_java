188e961c8352229f9e36c986a88e7b672e610366
DevEddi <scovinowork@gmail.com>
2022-01-06 10:35:46 -0300

    Moving pieces

--- src/main/java/chess/ChessPosition.java
+++ src/main/java/chess/ChessPosition.java
@@ -25,7 +25,7 @@ public class ChessPosition {
         return row;
     }
 
-    protected Position toPosotion(){
+    protected Position toPosition(){
         return new Position(8 -row, column - 'a');
     }
 
